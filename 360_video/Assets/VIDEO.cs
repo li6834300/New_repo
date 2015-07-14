1b790551f0fe59da1c6909338c8ca4e0eee0a324
Zhien <kenli@nyu.edu>
2015-07-14 10:15:06 -0400

    360_video_version_one

---
+++ 360_video/Assets/VIDEO.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (AudioSource))]
+
+public class VIDEO : MonoBehaviour {
+	public MovieTexture movTexture;
+	void Start() {
+		GetComponent<Renderer>().material.mainTexture = movTexture;
+		movTexture.Play();
+	}
+	
+	// Update is called once per frame
+
+}
